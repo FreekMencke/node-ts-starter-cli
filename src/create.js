c0492c17ef870b00df03bf795d48907279a71b2e
Freek Mencke <freek_mencke@outlook.com>
2023-01-11 21:45:26 +0100

    feat(create): initialize git repo after generating package.json

--- src/create.js
+++ src/create.js
@@ -12,9 +12,11 @@ module.exports = async function createProject(argv) {
   const projectFolder = path.join(cwd(), argv.name);
 
   // Create folder for project
+  console.log('Creating folder...');
   await mkdir(projectFolder);
 
   // Copy project base to folder
+  console.log('Creating root files...');
   await copy(path.join(packageFolder, 'templates/base'), projectFolder);
 
   // load package.json template, and modify it as we go.
@@ -24,12 +26,12 @@ module.exports = async function createProject(argv) {
   };
 
   // copy build and source files
-  console.log('Generating project base...');
+  console.log('Generating project files...');
   if (argv.minimal) {
     await copy(path.join(packageFolder, 'templates/build/minimal'), path.join(projectFolder, 'build'));
     await copy(path.join(packageFolder, 'templates/src/minimal'), path.join(projectFolder, 'src'));
   } else if (argv.maximal || argv.environments) {
-    console.log('Adding environment feature...');
+    console.log('Adding environments...');
     await copy(path.join(packageFolder, 'templates/build/environments'), path.join(projectFolder, 'build'));
     await copy(path.join(packageFolder, 'templates/src/environments'), path.join(projectFolder, 'src'));
   } else {
@@ -39,14 +41,14 @@ module.exports = async function createProject(argv) {
 
   // copy linting files & package.json
   if (argv.maximal || argv.eslint) {
-    console.log('Adding linting feature...');
+    console.log('Adding linting...');
     await copy(path.join(packageFolder, 'templates/eslint/default'), projectFolder);
     packageJson = merge(packageJson, require(path.join(packageFolder, 'templates/eslint/package.json')));
   }
 
   // copy docker files && package.json
   if (argv.maximal || argv.docker) {
-    console.log('Adding docker feature...');
+    console.log('Adding Docker...');
     if (argv.maximal || argv.eslint) {
       await copy(path.join(packageFolder, 'templates/docker/eslint'), projectFolder);
     } else {
@@ -57,10 +59,10 @@ module.exports = async function createProject(argv) {
 
   // copy github-action files && package.json
   if (argv.maximal || argv.githubAction) {
-    console.log('Adding github-action feature...');
+    console.log('Adding github-action...');
     if (argv.maximal || argv.docker) {
       await copy(path.join(packageFolder, 'templates/github-action/docker'), path.join(projectFolder, '.github/workflows'));
-    } else if (argv.eslint)  {
+    } else if (argv.eslint) {
       await copy(path.join(packageFolder, 'templates/github-action/eslint'), path.join(projectFolder, '.github/workflows'));
     } else {
       await copy(path.join(packageFolder, 'templates/github-action/default'), path.join(projectFolder, '.github/workflows'));
@@ -69,21 +71,35 @@ module.exports = async function createProject(argv) {
 
   // copy cz package.json
   if (argv.maximal || argv.commitizen) {
-    console.log('Adding commitizen feature...');
+    console.log('Adding commitizen...');
     packageJson = merge(packageJson, require(path.join(packageFolder, 'templates/commitizen/package.json')));
   }
 
-  // final step is to write our modified package.json template to our project folder, and do an install.
-  writeFile(path.join(projectFolder, 'package.json'), JSON.stringify(packageJson, null, 2).replaceAll(/\$npm_package_name/g, packageJson.name));
+  // Write our modified package.json template to our project folder
+  console.log('Generating package.json...');
+  await writeFile(
+    path.join(projectFolder, 'package.json'),
+    JSON.stringify(packageJson, null, 2).replaceAll(/\$npm_package_name/g, packageJson.name),
+  );
 
+  console.log('Cleaning up generated package.json...');
+  // by removing a fake package, npm sorts the dependencies and devDependencies alphabetically.
+  await promisify(exec)('npm remove -S example; npm remove -D example', { cwd: projectFolder });
+
+  // initialize git repo
   try {
-    console.log('Installing packages...');
-    // by removing a fake package, npm sorts the dependencies and devDependencies alphabetically.
-    await promisify(exec)('npm remove -S example; npm remove -D example', { cwd: projectFolder });
+    console.log('Initializing git repository...');
+    const { stdout } = await promisify(exec)('git init', { cwd: projectFolder });
+    process.stdout.write(stdout);
+  } catch (e) {
+    console.error(e);
+  }
 
-    const { stdout, stderr } = await promisify(exec)('npm i', { cwd: projectFolder });
-    console.log(stdout);
-    console.log(stderr);
+  // finally do an `npm i`
+  try {
+    console.log('Installing...');
+    const { stdout } = await promisify(exec)('npm i', { cwd: projectFolder });
+    process.stdout.write(stdout);
   } catch (e) {
     console.error(e);
   }
