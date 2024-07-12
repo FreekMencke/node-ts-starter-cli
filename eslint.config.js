bcbf3c72f8e66c71c8e9e822eb15ecb6faf445ef
Freek <freek_mencke@outlook.com>
2024-07-12 15:32:00 +0200

    build(eslint): fix some eslint config issues

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import globals from 'globals';
 export default [
   prettier,
   {
+    ignores: ['example/', 'templates/eslint/'],
+  },
+  {
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
@@ -19,6 +22,5 @@ export default [
       'prefer-const': 'error',
       'semi': ['error', 'always'],
     },
-    ignores: ['dist', 'node_modules', '.vscode', '/example/', '/templates/eslint'],
   },
 ];
--- templates/eslint/default/eslint.config.mjs
+++ templates/eslint/default/eslint.config.mjs
@@ -1,48 +1,52 @@
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  files: ['**/*.{js,cjs,mjs}', '**/*.ts'],
-  ignores: ['dist', 'node_modules', '.vscode'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: globals.node,
-    parser: tseslint.parser,
-    parserOptions: {
-      project: ['tsconfig.json', 'tsconfig.eslint.json'],
+export default tseslint.config(
+  {
+    ignores: ['/dist', 'node_modules', '.vscode'],
+  },
+  {
+    files: ['**/*.{js,cjs,mjs}', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.eslint.json'],
+      },
+    },
+    plugins: { '@typescript-eslint': tseslint.plugin },
+    rules: {
+      '@typescript-eslint/indent': ['error', 2],
+      '@typescript-eslint/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'semi',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'semi',
+            requireLast: false,
+          },
+        },
+      ],
+      '@typescript-eslint/quotes': [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+      '@typescript-eslint/semi': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'max-classes-per-file': 'off',
+      'no-console': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-redeclare': 'error',
+      'no-return-await': 'error',
+      'prefer-const': 'error',
     },
   },
-  plugins: { '@typescript-eslint': tseslint.plugin },
-  rules: {
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'semi',
-          requireLast: true,
-        },
-        singleline: {
-          delimiter: 'semi',
-          requireLast: false,
-        },
-      },
-    ],
-    '@typescript-eslint/quotes': [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-      },
-    ],
-    '@typescript-eslint/semi': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'max-classes-per-file': 'off',
-    'no-console': 'error',
-    'no-multiple-empty-lines': ['error', { max: 1 }],
-    'no-redeclare': 'error',
-    'no-return-await': 'error',
-    'prefer-const': 'error',
-  },
-});
+);
